25ecd9f768836475129b900d617d48ab2bafbbc5
BoWang <ralwayne@163.com>
2018-03-26 21:33:34 +0800

    init FrameBuff

---
+++ FrameBuffDemo/FrameBuffDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FrameBuffDemo
+//
+//  Created by BoWang on 2018/3/26.
+//  Copyright © 2018年 BoWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
